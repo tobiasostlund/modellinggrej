74708f099e4ada85184f50a313350d5c462e7671
tobiasostlund <tobias.ostlund@elev.ga.ntig.se>
2023-10-04 10:22:03 +0200

    finish

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,17 @@ public class Main {
         Car car2 = new Car(911,"stor bil","Axel Mannheimer",2);
 
         car1.speedUp(130);
-        System.out.println("Hastigheten är " + car1.getSpeed());
+        System.out.println("Bil 1:s hastighet är " + car1.getSpeed());
 
         car1.slowDown(50);
-        System.out.println("Hastigheten är " + car1.getSpeed());
+        System.out.println("Bil 1:s hastighet är " + car1.getSpeed());
 
         car1.stop();
-        System.out.println("Hastigheten är " + car1.getSpeed());
+        System.out.println("Bil 1:s hastighet är " + car1.getSpeed());
+
+        System.out.println("Bil 1:s ID är " + car1.getId());
+
+        System.out.println("Bil 2:s ID är " + car2.getId());
 
         car2.setOwner("Köppa Froppe");
 
